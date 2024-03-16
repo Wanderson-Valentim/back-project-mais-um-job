d7e2aa9593b8ce51683e90e505e27a6de74cb2eb
Wanderson-Valentim <wanderson200108@gmail.com>
2024-03-15 21:55:37 -0300

    init: configurações iniciais do express

---
+++ routes/users.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+
+router.get("/users", (req, res) => {
+  res.send("Todos os usuários");
+});
+
+router.get("/users/:id", (req, res) => {
+  res.send("Apenas um usuário");
+});
+
+router.post("/users", (req, res) => {
+  res.send("Criando usuário");
+});
+
+router.put("/users/:id", (req, res) => {
+  res.send("Atualizando usuário");
+});
+
+router.delete("/users/:id", (req, res) => {
+  res.send("Deletando usuário");
+});
+
+module.exports = router
